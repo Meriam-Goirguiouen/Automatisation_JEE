b0ae57df46d407549f9a4d5eab3b89ad33c4c169
Meriam-Goirguiouen <meriam.goirguiouen2002@gmail.com>
2025-04-24 10:59:53 +0100

    Initial commit depuis netbeans

---
+++ src/main/java/com/mycompany/automatisation_project/Automatisation_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.automatisation_project;
+
+/**
+ *
+ * @author univers
+ */
+public class Automatisation_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
